c02efa49db35e4e5cd25073e82e9d69ad06e5816
MR ROBOT <mr.robot.ethical.hackar@gmail.com>
2025-04-28 09:34:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1344,20 +1344,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if not files_:
             return await query.answer('Nᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪsᴛ.')
         files = files_
-        title = files["file_name"]
-        size = get_size(files["file_size"])
-        f_caption = files["caption"]
-        settings = await get_settings(query.message.chat.id)
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-            f_caption = f_caption
-        if f_caption is None:
-            f_caption = f"{files['file_name']}"
+      files = files_
+title = files["file_name"]
+size = get_size(files["file_size"])
+f_caption = files["caption"]
+
+if not f_caption:
+    f_caption = f"{files['file_name']}"
+
 
         try:
             if settings['is_shortlink'] and not await db.has_premium_access(query.from_user.id):
